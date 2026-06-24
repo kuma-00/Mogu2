c6ffe135a5cf542cc82b95d1a7b2f24991694a34
kuma-00 <kuma.gact@gmail.com>
2026-06-25 00:45:51 +0900

    chore: bump project version to 1.0.2 and remove unused hello function

--- crates/mogu-core/src/lib.rs
+++ crates/mogu-core/src/lib.rs
@@ -6,6 +6,3 @@ pub use processor::ImageProcessor;
 pub use detector::{FoodDetector, FoodDetectorConfig, FoodDetectionResult, PredictionLabel};
 pub use labels::{get_imagenet_label, classify_label, classify_food_kind, LabelCategory, FoodKind};
 
-pub fn hello() {
-    println!("Hello from Mogu!");
-}
